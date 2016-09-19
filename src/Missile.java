ca490990a01284268cc0beee2a1d233273928281
johnson1018 <ahhfxyl@gmail.com>
2016-09-19 22:08:23 +0800

    完成网络版坦克大战
    完成简单的网络对战

--- src/Missile.java
+++ src/Missile.java
@@ -3,50 +3,100 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.util.List;
 
-
-
+/**
+ * 代表子弹的类
+ * @author mashibing
+ *
+ */
 public class Missile {
+	/**
+	 * 子弹x方向的速度
+	 */
 	public static final int XSPEED = 10;
+	/**
+	 * 子弹y方向的速度
+	 */
 	public static final int YSPEED = 10;
-	
+	/**
+	 * 子弹的宽度
+	 */
 	public static final int WIDTH = 10;
+	/**
+	 * 子弹的高度
+	 */
 	public static final int HEIGHT = 10;
-	
+
+	private static int ID = 1;
+
 	TankClient tc;
-	
+
+	int tankId;
+
+	int id;
+
 	int x, y;
-	Tank.Direction dir = Tank.Direction.R;
+
+	Dir dir = Dir.R;
+
 	boolean live = true;
-	private boolean good;
+
+	boolean good;
 	
-	public Missile(int x, int y, boolean good, Tank.Direction dir) {
+	/**
+	 * 根据位置等属性构造子弹
+	 * @param tankId 所属坦克的id号(用于网络版)
+	 * @param x 子弹产生的x坐标
+	 * @param y 子弹产生的y坐标
+	 * @param good 子弹的立场是好还是坏
+	 * @param dir 子弹的方向
+	 * @see Dir
+	 */
+	
+	public Missile(int tankId, int x, int y, boolean good, Dir dir) {
+		this.tankId = tankId;
 		this.x = x;
 		this.y = y;
 		this.good = good;
 		this.dir = dir;
+		this.id = ID++;
 	}
 	
-	public Missile(int x, int y, boolean good, Tank.Direction dir, TankClient tc) {
-		this(x, y, good, dir);
+	/**
+	 * 根据位置和TankClient构建子弹
+	 * @param tankId
+	 * @param x
+	 * @param y
+	 * @param good
+	 * @param dir
+	 * @param tc 子弹构建的场所
+	 * @see TankClient
+	 */
+	public Missile(int tankId, int x, int y, boolean good, Dir dir,
+			TankClient tc) {
+		this(tankId, x, y, good, dir);
 		this.tc = tc;
 	}
 	
+	/**
+	 * 画出子弹
+	 * @param g 画笔
+	 */
 	public void draw(Graphics g) {
-		if(!live) {
+		if (!live) {
 			tc.missiles.remove(this);
 			return;
 		}
-		
+
 		Color c = g.getColor();
 		g.setColor(Color.BLACK);
 		g.fillOval(x, y, WIDTH, HEIGHT);
 		g.setColor(c);
-		
+
 		move();
 	}
 
 	private void move() {
-		switch(dir) {
+		switch (dir) {
 		case L:
 			x -= XSPEED;
 			break;
@@ -78,18 +128,29 @@ public class Missile {
 		case STOP:
 			break;
 		}
-		
-		if(x < 0 || y < 0 || x > TankClient.GAME_WIDTH || y > TankClient.GAME_HEIGHT) {
+
+		if (x < 0 || y < 0 || x > TankClient.GAME_WIDTH
+				|| y > TankClient.GAME_HEIGHT) {
 			live = false;
 		}
 	}
 	
+	/**
+	 * 取得子弹的外切方形
+	 * @return 子弹的外切Rectangle
+	 */
 	public Rectangle getRect() {
 		return new Rectangle(x, y, WIDTH, HEIGHT);
 	}
 	
+	/**
+	 * 检测子弹是否撞到坦克
+	 * @param t 被检测的坦克
+	 * @return 如果撞到返回true,否则返回false
+	 */
 	public boolean hitTank(Tank t) {
-		if(this.live && t.isLive() && this.good != t.good &&this.getRect().intersects(t.getRect())) {
+		if (this.live && t.isLive() && this.good != t.good
+				&& this.getRect().intersects(t.getRect())) {
 			this.live = false;
 			t.setLive(false);
 			tc.explodes.add(new Explode(x, y, tc));
@@ -98,9 +159,14 @@ public class Missile {
 		return false;
 	}
 	
+	/**
+	 * 检测是否撞到一系列坦克中的一个
+	 * @param tanks 被检测的坦克序列
+	 * @return 如果撞到其中一个,返回true,否则返回false
+	 */
 	public boolean hitTanks(List<Tank> tanks) {
-		for(int i=0; i<tanks.size(); i++) {
-			if(this.hitTank(tanks.get(i))) {
+		for (int i = 0; i < tanks.size(); i++) {
+			if (this.hitTank(tanks.get(i))) {
 				return true;
 			}
 		}
--- src/Tank.java
+++ src/Tank.java
@@ -2,90 +2,125 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
-import java.util.Random;
+//import java.util.Random;
 
+/**
+ * 代表坦克的类
+ * @author mashibing
+ *
+ */
 public class Tank {
+	int id;
+
 	public static final int XSPEED = 5;
+
 	public static final int YSPEED = 5;
-	
+
 	public static final int WIDTH = 30;
+
 	public static final int HEIGHT = 30;
-	
+
 	boolean good;
+
 	int x, y;
-	
-	private static Random r = new Random();
-	
+
+	//private static Random r = new Random();
+
 	private boolean live = true;
-	
-	private int step = r.nextInt(12) + 3;
-	
+
+	//private int step = r.nextInt(12) + 3;
+
 	TankClient tc;
-	
+
 	boolean bL, bU, bR, bD;
-	enum Direction {L, LU, U, RU, R, RD, D, LD, STOP};
+
+	Dir dir = Dir.STOP;
+
+	Dir ptDir = Dir.D;
 	
-	Direction dir = Direction.STOP;
-	Direction ptDir = Direction.D;
-	
+	/**
+	 * 根据位置和好坏构建坦克
+	 * @param x
+	 * @param y
+	 * @param good
+	 */
 	public Tank(int x, int y, boolean good) {
 		this.x = x;
 		this.y = y;
 		this.good = good;
 	}
 	
-	public Tank(int x, int y, boolean good, Direction dir, TankClient tc) {
+	/**
+	 * 根据相关属性构建坦克
+	 * @param x
+	 * @param y
+	 * @param good
+	 * @param dir
+	 * @param tc 游戏的场所
+	 */
+	public Tank(int x, int y, boolean good, Dir dir, TankClient tc) {
 		this(x, y, good);
 		this.dir = dir;
 		this.tc = tc;
 	}
 	
+	/**
+	 * 画出坦克
+	 * @param g 画笔
+	 */
 	public void draw(Graphics g) {
-		if(!live) {
-			if(!good) {
+		if (!live) {
+			if (!good) {
 				tc.tanks.remove(this);
 			}
 			return;
 		}
-		
+
 		Color c = g.getColor();
-		if(good) g.setColor(Color.RED);
-		else g.setColor(Color.BLUE);
+		if (good)
+			g.setColor(Color.RED);
+		else
+			g.setColor(Color.BLUE);
 		g.fillOval(x, y, WIDTH, HEIGHT);
+		g.drawString("id:" + id, x, y - 10);
 		g.setColor(c);
-		
-		switch(ptDir) {
-		case L:	
-			g.drawLine(x + WIDTH/2, y + HEIGHT/2, x, y + HEIGHT/2);
+
+		switch (ptDir) {
+		case L:
+			g.drawLine(x + WIDTH / 2, y + HEIGHT / 2, x, y + HEIGHT / 2);
 			break;
 		case LU:
-			g.drawLine(x + WIDTH/2, y + HEIGHT/2, x, y);
+			g.drawLine(x + WIDTH / 2, y + HEIGHT / 2, x, y);
 			break;
 		case U:
-			g.drawLine(x + WIDTH/2, y + HEIGHT/2, x + WIDTH/2, y);
+			g.drawLine(x + WIDTH / 2, y + HEIGHT / 2, x + WIDTH / 2, y);
 			break;
 		case RU:
-			g.drawLine(x + WIDTH/2, y + HEIGHT/2, x + WIDTH, y);
+			g.drawLine(x + WIDTH / 2, y + HEIGHT / 2, x + WIDTH, y);
 			break;
 		case R:
-			g.drawLine(x + WIDTH/2, y + HEIGHT/2, x + WIDTH, y + HEIGHT/2);
+			g
+					.drawLine(x + WIDTH / 2, y + HEIGHT / 2, x + WIDTH, y
+							+ HEIGHT / 2);
 			break;
 		case RD:
-			g.drawLine(x + WIDTH/2, y + HEIGHT/2, x + WIDTH, y + HEIGHT);
+			g.drawLine(x + WIDTH / 2, y + HEIGHT / 2, x + WIDTH, y + HEIGHT);
 			break;
 		case D:
-			g.drawLine(x + WIDTH/2, y + HEIGHT/2, x + WIDTH/2, y + HEIGHT);
+			g
+					.drawLine(x + WIDTH / 2, y + HEIGHT / 2, x + WIDTH / 2, y
+							+ HEIGHT);
 			break;
 		case LD:
-			g.drawLine(x + WIDTH/2, y + HEIGHT/2, x, y + HEIGHT);
+			g.drawLine(x + WIDTH / 2, y + HEIGHT / 2, x, y + HEIGHT);
 			break;
 		}
-		
+
 		move();
 	}
-
+	
 	private void move() {
-		switch(dir) {
+		switch (dir) {
 		case L:
 			x -= XSPEED;
 			break;
@@ -117,28 +152,32 @@ public class Tank {
 		case STOP:
 			break;
 		}
-		
-		if(dir != Direction.STOP) {
+
+		if (dir != Dir.STOP) {
 			ptDir = dir;
 		}
-		
-		if(x < 0) x = 0;
-		if(y < 30) y = 30;
-		if(x + WIDTH > TankClient.GAME_WIDTH) x = TankClient.GAME_WIDTH - WIDTH;
-		if(y + HEIGHT > TankClient.GAME_HEIGHT) y = TankClient.GAME_HEIGHT - HEIGHT;
-		
-		if(!good) {
-			if(step == 0) {
-				step = r.nextInt(12) + 3;
-				Direction[] dirs = Direction.values();
-				dir = dirs[r.nextInt(dirs.length)];
-			}
-			step --;
-			if(r.nextInt(40) > 38) this.fire();
-		}
-		
-	}
 
+		if (x < 0)
+			x = 0;
+		if (y < 30)
+			y = 30;
+		if (x + WIDTH > TankClient.GAME_WIDTH)
+			x = TankClient.GAME_WIDTH - WIDTH;
+		if (y + HEIGHT > TankClient.GAME_HEIGHT)
+			y = TankClient.GAME_HEIGHT - HEIGHT;
+
+		/*
+		 * if(!good) { if(step == 0) { step = r.nextInt(12) + 3; Dir[] dirs =
+		 * Dir.values(); dir = dirs[r.nextInt(dirs.length)]; } step --;
+		 * if(r.nextInt(40) > 38) this.fire(); }
+		 */
+
+	}
+	
+	/**
+	 * 键按下的消息处理
+	 * @param e 按键事件
+	 */
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
 		switch (key) {
@@ -159,17 +198,37 @@ public class Tank {
 	}
 
 	private void locateDirection() {
-		if(bL && !bU && !bR && !bD) dir = Direction.L;
-		else if(bL && bU && !bR && !bD) dir = Direction.LU;
-		else if(!bL && bU && !bR && !bD) dir = Direction.U;
-		else if(!bL && bU && bR && !bD) dir = Direction.RU;
-		else if(!bL && !bU && bR && !bD) dir = Direction.R;
-		else if(!bL && !bU && bR && bD) dir = Direction.RD;
-		else if(!bL && !bU && !bR && bD) dir = Direction.D;
-		else if(bL && !bU && !bR && bD) dir = Direction.LD;
-		else if(!bL && !bU && !bR && !bD) dir = Direction.STOP;
-	}
+		Dir oldDir = this.dir;
 
+		if (bL && !bU && !bR && !bD)
+			dir = Dir.L;
+		else if (bL && bU && !bR && !bD)
+			dir = Dir.LU;
+		else if (!bL && bU && !bR && !bD)
+			dir = Dir.U;
+		else if (!bL && bU && bR && !bD)
+			dir = Dir.RU;
+		else if (!bL && !bU && bR && !bD)
+			dir = Dir.R;
+		else if (!bL && !bU && bR && bD)
+			dir = Dir.RD;
+		else if (!bL && !bU && !bR && bD)
+			dir = Dir.D;
+		else if (bL && !bU && !bR && bD)
+			dir = Dir.LD;
+		else if (!bL && !bU && !bR && !bD)
+			dir = Dir.STOP;
+
+		if (dir != oldDir) {
+			TankMoveMsg msg = new TankMoveMsg(id, x, y, dir, ptDir);
+			tc.nc.send(msg);
+		}
+	}
+	
+	/**
+	 * 键抬起的消息处理
+	 * @param e 抬键消息
+	 */
 	public void keyReleased(KeyEvent e) {
 		int key = e.getKeyCode();
 		switch (key) {
@@ -191,23 +250,42 @@ public class Tank {
 		}
 		locateDirection();
 	}
-	
+
 	private Missile fire() {
-		int x = this.x + WIDTH/2 - Missile.WIDTH/2;
-		int y = this.y + HEIGHT/2 - Missile.HEIGHT/2;
-		Missile m = new Missile(x, y, this.good, this.ptDir, this.tc);
+		if (!live)
+			return null;
+
+		int x = this.x + WIDTH / 2 - Missile.WIDTH / 2;
+		int y = this.y + HEIGHT / 2 - Missile.HEIGHT / 2;
+		Missile m = new Missile(id, x, y, this.good, this.ptDir, this.tc);
 		tc.missiles.add(m);
+
+		MissileNewMsg msg = new MissileNewMsg(m);
+		tc.nc.send(msg);
+
 		return m;
 	}
 	
+	/**
+	 * 取得坦克的外切方形
+	 * @return 坦克的外切Rectangle
+	 */
 	public Rectangle getRect() {
 		return new Rectangle(x, y, WIDTH, HEIGHT);
 	}
-
+	
+	/**
+	 * 检测坦克是否还活着
+	 * @return
+	 */
 	public boolean isLive() {
 		return live;
 	}
-
+	
+	/**
+	 * 设定坦克的生死状态
+	 * @param live
+	 */
 	public void setLive(boolean live) {
 		this.live = live;
 	}
